58adbe9511303ee68d1c9cb76f46586d73a8e157
yamachan32 <apropiado.para.m@gmail.com>
2022-05-16 14:18:11 +0900

    commit

---
+++ src/main/kotlin/com/book/manager/domain/model/BookWithRentalRecord.kt
@@ -0,0 +1,14 @@
+package com.book.manager.domain.model
+
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+data class BookWithRentalRecord (
+    var id: Long? = null,
+    var title: String? = null,
+    var author: String? = null,
+    var releaseDate: LocalDate? = null,
+    var userID: Long? = null,
+    var rentalDatetime: LocalDateTime? = null,
+    var returnDateline: LocalDateTime? = null
+)
