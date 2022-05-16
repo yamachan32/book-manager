18c2d9e0646fc1abcad83f66b376facbebac6600
yamachan32 <apropiado.para.m@gmail.com>
2022-05-16 16:27:10 +0900

    commit

---
+++ src/main/kotlin/com/book/manager/infrastructure/database/mapper/custom/BookWithRentalMapper.kt
@@ -0,0 +1,29 @@
+package com.book.manager.infrastructure.database.mapper.custom
+
+import com.book.manager.domain.model.BookWithRentalRecord
+import com.mysql.cj.xdevapi.SelectStatement
+import org.apache.ibatis.annotations.Mapper
+import org.apache.ibatis.annotations.Result
+import org.apache.ibatis.annotations.Results
+import org.apache.ibatis.annotations.SelectProvider
+import org.apache.ibatis.type.JdbcType
+import org.mybatis.dynamic.sql.select.render.SelectStatementProvider
+import org.mybatis.dynamic.sql.util.SqlProviderAdapter
+import org.springframework.jdbc.core.SqlProvider
+
+@Mapper
+interface BookWithRentalMapper {
+    @SelectProvider(type = SqlProviderAdapter::class, method="select")
+    @Results(
+        id = "BookWithRecordResult", value = [
+            Result(column = "id", property = "id", jdbcType = JdbcType.BIGINT, id = true),
+            Result(column = "title", property = "title", jdbcType = JdbcType = JdbcType.VARCHAR),
+            Result(column = "author", property = "author", jdbcType = JdbcType.VARCHAR),
+            Result(column = "release_date", property = "releaseDate", jdbcType = JdbcType.DATE),
+            Result(column = "user_id", property = "userId", jdbcType = JdbcType.BIGINT),
+            Result(column = "rental_datetime", property = "rentalDatetime", jdbcType = JdbcType.TIMESTAMP),
+            Result(column = "return_deadline", property = "returanDeadline", jdbcType = JdbcType.TIMESTAMP)
+        ]
+    )
+    fun selectMany(selectStatement: SelectStatementProvider): List<BookWithRentalRecord>
+}
